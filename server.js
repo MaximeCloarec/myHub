c0a3538ca6fbb71974a6cecad94d0c1cb62ad8ac
MaximeCloarec <156484577+MaximeCloarec@users.noreply.github.com>
2025-06-01 09:45:53 +0200

    First app

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3050;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/foodList",require("./apps/foodList/app.js"));
+
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`My Hub is listening on port ${port}`);
+    console.log("MyHub is running on port " + port);
 });
